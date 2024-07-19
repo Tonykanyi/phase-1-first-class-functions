b26ecdc8df3c175d4c72e51ae61a6e67012116ee
Tony <kanyianthony79@gmail.com>
2024-07-19 13:09:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+      return function named(){ console.log(" Wow!")}
+  }
+  
+  function returnsAnAnonymousFunction() {
+      return function(){ console.log(" Wow!")}
+  }
